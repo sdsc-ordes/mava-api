e105a89ad6114585493d60143aebf822ef66c1f5
Stefan Milosavljevic <stefan.milosavljevic@sdsc.ethz.ch>
2026-01-07 16:44:42 +0100

    chore(tests): add_data unit test

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,6 +1,6 @@
 import pytest
 from mava.graph.builder import validate_mapping, GraphBuilder, MAVA, EX
-from rdflib import URIRef, Literal, RDF
+from rdflib import Literal, RDF, XSD
 
 def test_validate_mapping_valid():
     mapping = {"time_column": "t", "value_column": "v"}
@@ -47,3 +47,55 @@ def test_add_series(mocker, has_duration, expected_type):
     ]
     instance.g.add.assert_has_calls(calls)
     instance.g.add.call_count == 3
+
+@pytest.mark.parametrize("has_duration,value_type,expected_triples", [
+    # Case 1: With duration
+    (True, None, [
+        (MAVA.startTime, Literal(10, datatype=XSD.decimal)),
+        (MAVA.endTime, Literal(15, datatype=XSD.decimal)),
+        (MAVA.stringValue, Literal("abc", datatype=XSD.string)),
+    ]),
+    # Case 2: Without duration, numeric value
+    (False, "numeric", [
+        (MAVA.atTime, Literal(10, datatype=XSD.decimal)),
+        (MAVA.numericValue, Literal(42, datatype=XSD.decimal)),
+    ]),
+    # Case 3: Without duration, list value
+    (False, "list", [
+        (MAVA.atTime, Literal(10, datatype=XSD.decimal)),
+        (MAVA.listValue, Literal(str([1, 2, 3]), datatype=RDF.List)),
+    ]),
+])
+def test_add_data(mocker, has_duration, value_type, expected_triples):
+    instance = GraphBuilder()
+    instance.g = mocker.MagicMock()
+
+    series_id = "fixed-uuid"
+    data_point_idx = 1
+    data_point_uri = EX[f"{series_id}_point_{data_point_idx}"]
+    series_uri = EX[f"{series_id}"]
+
+    data = [{"time": 10}]
+    if has_duration:
+        data[0].update({"value": "abc", "duration": 5})
+    else:
+        data[0]["value"] = 42 if value_type == "numeric" else [1, 2, 3]
+
+    mapping = {
+        "time_column": "time",
+        "value_column": "value",
+        "value_type": value_type,
+        "duration_column": "duration" if has_duration else None,
+    }
+
+    instance.add_data(series_id, data, mapping, has_duration)
+
+    expected = [
+        ((data_point_uri, MAVA.belongsToSeries, series_uri), {}),
+    ]
+
+    for pred, obj in expected_triples:
+        expected.append(((data_point_uri, pred, obj), {}))
+
+    instance.g.add.assert_has_calls(expected)
+    assert instance.g.add.call_count == len(expected)
